9f1d5f6cc648c08ada7b812f77e6a9d685856b40
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-04-10 17:32:06 -0600

    Password hasheado y generacion de token

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 import Usuario from "../models/Usuario.js"
+import generarId from "../helpers/generarId.js"
 
 export const crearUsuario = async (req, res) => {
   const { email } = req.body
@@ -15,8 +16,11 @@ export const crearUsuario = async (req, res) => {
   try {
     //--> Crea un usuario apartir de lo que recibio
     const usuario = new Usuario(req.body)
+    usuario.token = generarId()
+
     //--> Guardar registro en DB
     const usuarioAlmacenado = await usuario.save()
+
     //--> Muestra el usuario que se creo
     res.json(usuarioAlmacenado)
 
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-// import bcrypt from "bcrypt"
+import bcrypt from "bcrypt"
 
 //--> Definir estructura de usuario
 //--> Va a interactuar con la DB
@@ -34,6 +34,18 @@ const usuarioSchema = mongoose.Schema(
   timestamps: true,
 }
 )
+
+//--> Se ejecuta antes de guardar el registro en la DB
+usuarioSchema.pre('save', async function (next) {
+  //--> Si no esta modificando la contraseña no se hara nada
+  if (!this.isModified("password")) { next() }
+
+  //--> N. de rondas
+  const salt = await bcrypt.genSalt(10)
+
+  //--> Aplicar has creado a contraseña recibida
+  this.password = bcrypt.hash(this.password, salt)
+})
 
 //--> Crear modelo usuario / tabla usuario
 const Usuario = mongoose.model("Usuario", usuarioSchema)
